780cfa9319262d21cfe5f4567f1c8829bebfcbbd
caue peruque <caue19052003@gmail.com>
2023-10-02 15:50:31 -0300

    fix: copy clipboard

--- src/components/ShareRecipeBtn.jsx
+++ src/components/ShareRecipeBtn.jsx
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import copy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
 import './style/ButtonsSoF.css';
-
-// const copy = require('clipboard-copy');
 
 class ShareRecipeBtn extends Component {
   state = {
@@ -13,11 +12,19 @@ class ShareRecipeBtn extends Component {
 
   handleClick = (e) => {
     e.preventDefault();
-    const { path } = this.props;
-    const path1 = path.split('/');
-    console.log(path1[2]);
-    console.log(path);
-    copy(`http://localhost:3000/${path1[1]}/${path1[2]}`);
+    // const { path } = this.props;
+    const limitPath = 5;
+    const limitPath2 = 6;
+
+    const relativePath = window.location.href.split('/');
+    const toto = relativePath.length >= limitPath2 ? relativePath.slice(0, limitPath).join().replace(/,/g, '/') : relativePath.join().replace(/,/g, '/');
+    // const testie = toto.join();
+    // const path1 = path.split('/');
+    // console.log(path1[2]);
+    // console.log(path);
+    copy(toto);
+    console.log(toto);
+
     this.setState({
       hidden: 'active',
     });
